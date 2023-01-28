c5a75033245799696480d478552a04e587df32ac
MARS <leprcoin@gmail.com>
2023-01-28 23:51:12 +0400

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+int n = 8;
+int[] arr = new int[n];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0,2);
+    Console.WriteLine (arr[i]);
+}
+
+//Console.WriteLine(string.Join(", ", arr));
