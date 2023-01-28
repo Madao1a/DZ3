e3ea28ce2a743608fcf40fb6c72c17c49e2e0c35
MARS <leprcoin@gmail.com>
2023-01-28 23:18:52 +0400

    task5

---
+++ task5/Program copy.cs
@@ -0,0 +1,9 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine ("Введите число N ");
+int N = int.Parse (Console.ReadLine());
+int res = 1;
+for (int i = 1; i <= N ; i++)
+{
+    res=res*i;
+}
+Console.WriteLine (res);
