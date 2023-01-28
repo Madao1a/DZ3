a9c734d506bba980675e6304aeb2aac37ad8e52a
MARS <leprcoin@gmail.com>
2023-01-28 23:00:44 +0400

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine ("Введие число ");
+int A = int.Parse (Console.ReadLine());
+int sum = A%10;
+for (int i = A; i >= 10 ||i <=-10; i=i/10)
+{
+    sum =sum + i%10;
+}
+Console.WriteLine (sum);
