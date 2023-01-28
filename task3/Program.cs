e1cf149e6d785ec79b4f35105351e89f0b88e70f
MARS <leprcoin@gmail.com>
2023-01-28 22:06:54 +0400

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+Console.WriteLine ("Введите число A ");
+int A = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введтие число B");
+int B = int.Parse (Console.ReadLine());
+int Res = 1;
+for (int i = 0; i < B; i++)
+{
+    
+    Res =Res * A;
+}
+Console.WriteLine (Res);
