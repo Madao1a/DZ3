770ae298ce0e749d8fcd98fd7378980d82406476
MARS <leprcoin@gmail.com>
2023-01-28 23:27:35 +0400

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine ("Введите число N ");
+int numbers = int.Parse (Console.ReadLine());
+for (int i = 1; i<=numbers ; i++)
+{
+    if (i%2==0)
+    {
+        Console.WriteLine (i*i*i);
+    }
+}
