e93d7e29f6523e6b5742103871c08c4d2d981941
MARS <leprcoin@gmail.com>
2023-01-29 15:49:21 +0400

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int[] numbers = new int[33];
+Random rand = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]=rand.Next(1,100);
+}
+int sum = 0;
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (j%2!=0)
+    {
+        sum=sum+numbers[j];
+    }
+}
+Console.WriteLine (sum);
