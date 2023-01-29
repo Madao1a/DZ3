64034bd996ef6ffe6ef406e64e683e0372655689
MARS <leprcoin@gmail.com>
2023-01-29 23:13:06 +0400

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿//В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double[] numbers = new double[10];
+Random rand = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]= rand.Next(1,6);
+    Console.WriteLine (numbers[i]);
+}
+double min=numbers[1];
+double max=numbers[1];
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j]>max)
+    {
+        max=numbers[j];
+    }
+    if (numbers[j]<min)
+    {
+        min=numbers[j];
+    }
+}
+Console.WriteLine (max-min);
