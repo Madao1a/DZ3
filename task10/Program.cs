7caa7758e5394a11bb010ffcbb0aac80f05c480c
MARS <leprcoin@gmail.com>
2023-01-29 15:38:42 +0400

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int [] numbers = new int[123];
+int count = 0;
+for (int i = 10; i <= 99; i++)
+{
+    count = count+1;
+}
+Console.WriteLine (count*2);
+
+
