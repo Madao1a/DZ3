fac3214ef161aa8d8ae0af3c8eec94897eab3c1a
MARS <leprcoin@gmail.com>
2023-01-29 16:13:58 +0400

    task12

---
+++ task12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int[] numbers = new int[33];
+Random rand = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]=rand.Next(1,20);
+}
+int size = numbers.Length - 1;
+for (int j = 0; j < numbers.Length; j++)
+{
+    int res = numbers[j]*numbers[size];
+    Console.WriteLine (res);
+    size=size-1;
+}
